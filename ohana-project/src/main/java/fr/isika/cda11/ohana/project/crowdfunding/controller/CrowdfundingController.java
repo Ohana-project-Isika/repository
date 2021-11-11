ae2e01190b8c17cb04297d139483e8db03b0c9bd
Jordan <petitjord@gmail.com>
2021-11-11 17:45:27 +0100

    MAJ Feature jordan

--- ohana-project/src/main/java/fr/isika/cda11/ohana/project/crowdfunding/controller/CrowdfundingController.java
+++ ohana-project/src/main/java/fr/isika/cda11/ohana/project/crowdfunding/controller/CrowdfundingController.java
@@ -1,5 +1,92 @@
 package fr.isika.cda11.ohana.project.crowdfunding.controller;
 
-public class CrowdfundingController {
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import fr.isika.cda11.ohana.project.crowdfunding.models.Funding;
+import fr.isika.cda11.ohana.project.crowdfunding.models.Person;
+import fr.isika.cda11.ohana.project.crowdfunding.models.Project;
+import fr.isika.cda11.ohana.project.crowdfunding.repository.CrowdfundingRepository;
+
+
+
+@ManagedBean(name = "projectMB")
+@SessionScoped
+public class CrowdfundingController {
+	
+	@Inject
+	CrowdfundingRepository cr;
+	
+	Project theProject = new Project();
+	Project newProject = new Project();
+	Person thePerson = new Person();
+	Funding theFunding = new Funding();
+
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Create " + this);
+		if (cr.findProjects().size() == 0) {
+			
+			Project p1 = new Project();
+			p1.setName("Architecture JEE");
+			p1.setFinancialGoal(500000);
+			p1.setDescription("gros projet");
+			cr.saveProject(p1);
+		}
+	}
+
+	public List<Project> getProjects() {
+		return cr.findProjects();
+	}
+	public List<Funding> getFundings() {
+		return cr.findFundings();
+	}
+
+
+	public Funding getTheFunding() {
+		return theFunding;
+	}
+
+	public Person getThePerson() {
+		return thePerson;
+	}
+
+	public Project getTheProject() {
+		return theProject;
+	}
+	
+	public Project getNewProject() {
+		return newProject;
+	}
+
+	public String show(Integer n) {
+		theProject = cr.findProject(n);
+		return "showProject";
+	}
+
+	public String deleteProject() {
+		cr.deleteProject(theProject);
+		return "projects";
+	}
+	public String save() {
+		cr.saveProject(theProject);
+		return "showProject";
+	}
+
+	public String newProject() {
+		cr.saveProject(newProject);
+		newProject = new Project();
+		return "projects";
+	}
+	
+	public String newFunding() {
+		cr.saveFunding(theFunding);
+		theFunding = new Funding();
+		return "customerProjectList";
+	}
 }
---
+++ ohana-project/src/main/java/fr/isika/cda11/ohana/project/crowdfunding/models/Funding.java
@@ -0,0 +1,59 @@
+package fr.isika.cda11.ohana.project.crowdfunding.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Funding {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private Integer fundingAmount;
+	
+	@ManyToOne
+	private Project project;
+	
+	
+	//private Person person;
+/*
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+*/
+	public Funding() {
+		
+	}
+
+	public Integer getFundingAmount() {
+		return fundingAmount;
+	}
+
+	public void setFundingAmount(Integer fundingAmount) {
+		this.fundingAmount = fundingAmount;
+	}
+/*
+	
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+	*/
+
+	public Long getId() {
+		return id;
+	}
+
+}
