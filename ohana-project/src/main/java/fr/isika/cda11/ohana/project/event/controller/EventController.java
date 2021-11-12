022b68b5768a1a7764c4ae63485d4dd43c9f496e
lorraine k <lokrinbarg@gmail.com>
2021-11-12 16:13:15 +0100

    creation de la page html AssociationCRUD.xhtml

--- ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventController.java
+++ ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventController.java
@@ -1,5 +1,39 @@
 package fr.isika.cda11.ohana.project.event.controller;
 
-public class EventController {
+import fr.isika.cda11.ohana.project.event.models.Event;
+import fr.isika.cda11.ohana.project.event.service.EventService;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+@Getter
+@Setter
+public class EventController implements Serializable {
+
+    private static final long serialVersionUID = -1606040173965309710L;
+
+    // Utilisé pour le formulaire de création d'évènement
+    private List<Event> events;
+
+    private Event event = new Event();
+
+    @Inject
+    private EventService eventService;
+
+    public void add() {
+        eventService.create(event);
+        this.events = eventService.findAll();
+        this.event = new Event();
+    }
+
+
+
 
 }
