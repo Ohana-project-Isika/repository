2fe3a731f08bf5d4aa01866077b1441e20507b79
lorraine k <lokrinbarg@gmail.com>
2021-11-12 21:23:07 +0100

    maj event repo datetime

--- ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventController.java
+++ ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventController.java
@@ -5,6 +5,9 @@ import fr.isika.cda11.ohana.project.event.service.EventService;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -27,11 +30,32 @@ public class EventController implements Serializable {
     @Inject
     private EventService eventService;
 
-    public void add() {
+    @PostConstruct
+    private void init() {
+        events = eventService.findAll();
+    }
+
+    public void addEvent() {
         eventService.create(event);
         this.events = eventService.findAll();
         this.event = new Event();
     }
+
+    public void updateEvent() {
+        events.forEach(eventService::update);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Update successful"));
+    }
+
+    public void onTabChange(Event event) {
+        FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onTabClose(Event event) {
+        FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
 
 
 
