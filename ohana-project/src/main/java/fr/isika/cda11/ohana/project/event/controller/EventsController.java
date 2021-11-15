94820b37caa9e706a85b0c1ff2759fb1c6ed1f51
lorraine k <lokrinbarg@gmail.com>
2021-11-15 12:43:27 +0100

    modification page read et read by id

--- ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventsController.java
+++ ohana-project/src/main/java/fr/isika/cda11/ohana/project/event/controller/EventsController.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -19,21 +18,21 @@ import java.util.Map;
 @ViewScoped
 @Getter
 @Setter
-public class EventControllerRead implements Serializable {
+public class EventsController implements Serializable {
 
     private static final long serialVersionUID = -1606040173965309710L;
 
-    private Event event;
+    private List<Event> events;
 
     @Inject
     private EventService eventService;
 
-    public void findEvent() {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        Map<String, String> parameterMap =
-                facesContext.getExternalContext().getRequestParameterMap();
-        Long eventId = Long.parseLong(parameterMap.get("eventId"));
-        event = eventService.findById(eventId);
+    @PostConstruct
+    private void init() {
+        events = eventService.findAll();
     }
 
+    public String viewEvent() {
+        return "AssociationCRUD_readById";
+    }
 }
